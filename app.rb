269a3a5e615c8e993027811955c5575ba4b9d84e
cyantis <cyantis@gmail.com>
2019-05-13 20:39:42 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is Jakob.'
+  end
+
+  get  '/hometown' do
+    'My hometown is Gardner, CO.'
+  end
+
+  get  '/favorite-song' do
+    "My favorite song is 'I really should\'ve gone out last night' by Dirty Three."
+  end
+
 end
