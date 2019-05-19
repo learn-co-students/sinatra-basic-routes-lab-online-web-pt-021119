b3a3b9b73eb7c19d5434db47d097f2e68d41540e
Victor Gonzalez <victorggrodriguez@gmail.com>
2019-05-19 17:37:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Victor"
+  end
+
+  get '/hometown' do
+    "My hometown is San Juan"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Magic from Coldplay"
+  end
+
 end
