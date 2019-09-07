e4f7c2f9c3184160c7be819fe4da19d80b90def8
Chance Smith <chancesmith97@hotmail.com>
2019-09-07 21:42:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/name' do 
+    "My name is Chance."
+  end
+  
+  get '/hometown' do 
+    "My hometown is Rapid City."
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Shortcuts and Deadends."
+  end
+  
 end
